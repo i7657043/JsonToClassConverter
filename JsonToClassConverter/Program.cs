8cd2189f05feb172aab592e5da7c7219e9641478
Jordan Griffiths <jordan_griff2012@hotmail.com>
2024-11-06 00:31:31 +0000

    starting

--- JsonToClassConverter/Program.cs
+++ JsonToClassConverter/Program.cs
@@ -6,94 +6,39 @@ internal class Program
     {
         string json = JsonSerializer.Serialize(new Person
         {
-            FirstName = "hi",
-            SecondName = "hello",
-            NonName = null,
+            FirstName = "outer",
+            SecondName = "outer",
             Age = 21,
-            Male = false,
-            Children = new List<Child>
+            AChild = new Child
             {
-                new Child
+                ChildName = "nest 1",
+                Children = new List<Child>
                 {
-                    NonName = null,
-                    ChildName = "yoyo",
-                    ChildOfChild = new ChildOfChild
+                    new Child
                     {
-                        ChildOfChildName = "nest",
-                        Age = 21,
-                        Dob = DateTime.Now,
-                        Male = true
-                    },
-                    Children = new List<Child>
-                    {
-                        new Child
-                        {
-                            NonName = null,
-                            ChildName = "nest 1",
-                            ChildOfChild = new ChildOfChild
-                            {
-                                ChildOfChildName = "nest 1",
-                                Age = 21,
-                                Dob = DateTime.Now,
-                                Male = true
-                            }
-                        },
-                        new Child
-                        {
-                            NonName = null,
-                            ChildName = "anotherChild",
-                            Age = 21,
-                            Dob = DateTime.Now,
-                            Children = new List<Child>
-                            {
-                                new Child
-                                {
-                                    NonName = null,
-                                    ChildName = "nest 2",
-                                    ChildOfChild = new ChildOfChild
-                                    {
-                                        ChildOfChildName = "nest 2",
-                                        Age = 21,
-                                        Dob = DateTime.Now,
-                                        Male = true
-                                    }
-                                },
-                                new Child
-                                {
-                                    NonName = null,
-                                    ChildName = "anotherChild",
-                                    Age = 21,
-                                    Dob = DateTime.Now
-                                }
-                            }
-                        }
+                        ChildName = "nest 2",
                     }
-                },
-                new Child
-                {
-                    NonName = null,
-                    ChildName = "nest",
-                    Age = 21,
-                    Dob = DateTime.Now
                 }
             },
-            Child = new Child
-            {
-                NonName = null,
-                ChildName = "extra",
-                ChildOfChild = new ChildOfChild
-                {
-                    ChildOfChildName = "extra child of child"
-                },
-                Age = 21,
-                Dob = DateTime.Now,
-                Male = true
-            }
+            //Children = new List<Child>
+            //{
+            //    new Child
+            //    {
+            //        ChildName = "nest 1",
+            //        Children = new List<Child>
+            //        {
+            //            new Child
+            //            {
+            //                ChildName = "nest 2",
+            //            }
+            //        }
+            //    },
+            //}
         });
 
         DocumentModel model = GetDocumentModel(json);
 
-        PrintModel(model);
+        //PrintModel(model);
     }
 
     private static DocumentModel GetDocumentModel(string json)
@@ -108,7 +53,8 @@ internal class Program
             switch (property.Value.ValueKind)
             {
                 case JsonValueKind.Object:
-                    var childObject = new ChildObject(property.Name)
+                case JsonValueKind.Array:
+                    var childObject = new ChildObject(property.Name, GetValueType(property.Value.ValueKind, property.Value.GetString()))
                     {
                         Fields = ParseJson(property.Value).Fields,
                         ChildObjects = ParseJson(property.Value).ChildObjects,
@@ -117,20 +63,15 @@ internal class Program
                     model.ChildObjects.Add(childObject);
                     break;
 
-                case JsonValueKind.Array:
-                    var arrayField = ParseArray(property.Name, property.Value);
-                    model.Arrays.Add(arrayField);
-                    break;
-
                 case JsonValueKind.String:
                     var fieldType = GetValueType(property.Value.ValueKind, property.Value.GetString());
                     model.Fields.Add(new Field(property.Name, fieldType));
                     break;
 
-                case JsonValueKind.Null:
-                    // Add a nullable field type for null values
-                    model.Fields.Add(new Field(property.Name, typeof(Nullable<>)));
-                    break;
+                //case JsonValueKind.Null:
+                //    // Add a nullable field type for null values
+                //    model.Fields.Add(new Field(property.Name, typeof(Nullable<>)));
+                //    break;
 
                 default:
                     if (IsValueType(property.Value.ValueKind))
@@ -142,25 +83,6 @@ internal class Program
         }
 
         return model;
-    }
-
-    private static ArrayField ParseArray(string name, JsonElement arrayElement)
-    {
-        var arrayField = new ArrayField(name);
-
-        foreach (var item in arrayElement.EnumerateArray())
-        {
-            if (IsValueType(item.ValueKind))
-            {
-                arrayField.PrimitiveValues.Add(item.ToString());
-            }
-            else if (item.ValueKind == JsonValueKind.Object)
-            {
-                arrayField.ObjectValues.Add(ParseJson(item));
-            }
-        }
-
-        return arrayField;
     }
 
     private static bool IsValueType(JsonValueKind valueKind) =>
@@ -184,35 +106,35 @@ internal class Program
         ? typeof(DateTime)
         : typeof(string);
 
-    private static void PrintModel(DocumentModel model, int indent = 0)
-    {
-        string indentStr = new string(' ', indent);
-        Console.WriteLine($"{indentStr}DocumentModel:");
+    //private static void PrintModel(DocumentModel model, int indent = 0)
+    //{
+    //    string indentStr = new string(' ', indent);
+    //    Console.WriteLine($"{indentStr}DocumentModel:");
 
-        foreach (var field in model.Fields)
-        {
-            Console.WriteLine($"{indentStr}  Field: {field.Name} ({field.Type.Name})");
-        }
+    //    foreach (var field in model.Fields)
+    //    {
+    //        Console.WriteLine($"{indentStr}  Field: {field.Name} ({field.Type.Name})");
+    //    }
 
-        foreach (var array in model.Arrays)
-        {
-            Console.WriteLine($"{indentStr}  ArrayField: {array.Name}");
-            foreach (var primitiveValue in array.PrimitiveValues)
-            {
-                Console.WriteLine($"{indentStr}    PrimitiveValue: {primitiveValue}");
-            }
-            foreach (var obj in array.ObjectValues)
-            {
-                PrintModel(obj, indent + 4);
-            }
-        }
+    //    foreach (var array in model.Arrays)
+    //    {
+    //        Console.WriteLine($"{indentStr}  ArrayField: {array.Name}");
+    //        foreach (var primitiveValue in array.PrimitiveValues)
+    //        {
+    //            Console.WriteLine($"{indentStr}    PrimitiveValue: {primitiveValue}");
+    //        }
+    //        foreach (var obj in array.ObjectValues)
+    //        {
+    //            PrintModel(obj, indent + 4);
+    //        }
+    //    }
 
-        foreach (var child in model.ChildObjects)
-        {
-            Console.WriteLine($"{indentStr}  ChildObject: {child.Name}");
-            PrintModel(new DocumentModel { Fields = child.Fields, ChildObjects = child.ChildObjects, Arrays = child.Arrays }, indent + 4);
-        }
-    }
+    //    foreach (var child in model.ChildObjects)
+    //    {
+    //        Console.WriteLine($"{indentStr}  ChildObject: {child.Name}");
+    //        PrintModel(new DocumentModel { Fields = child.Fields, ChildObjects = child.ChildObjects, Arrays = child.Arrays }, indent + 4);
+    //    }
+    //}
 
     // Model classes
     public class DocumentModel
@@ -224,12 +146,14 @@ internal class Program
 
     public class ChildObject : DocumentModel
     {
-        public ChildObject(string name)
+        public ChildObject(string name, Type type)
         {
             Name = name;
+            Type = type;
         }
 
         public string Name { get; set; }
+        public Type Type { get; set; }
     }
 
     public class Field
@@ -261,30 +185,14 @@ internal class Program
     {
         public string FirstName { get; set; }
         public string SecondName { get; set; }
-        public string? NonName { get; set; } = null;
         public int Age { get; set; }
-        public bool Male { get; set; }
-        public DateTime Dob { get; set; }
-        public List<Child> Children { get; set; }
-        public Child Child { get; set; }
+        public Child AChild { get; set; }
+        //public List<Child> Children { get; set; }
     }
 
     public class Child
     {
         public string ChildName { get; set; }
-        public string? NonName { get; set; } = null;
-        public int Age { get; set; }
-        public bool Male { get; set; }
-        public DateTime Dob { get; set; }
         public List<Child> Children { get; set; }
-        public ChildOfChild ChildOfChild { get; set; }
-    }
-
-    public class ChildOfChild
-    {
-        public int Age { get; set; }
-        public bool Male { get; set; }
-        public DateTime Dob { get; set; }
-        public string ChildOfChildName { get; set; }
     }
 }
